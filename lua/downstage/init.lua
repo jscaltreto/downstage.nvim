be7863cc5c117f4550b4ea76832b56e09f6dbf02
Jake Scaltreto <jake@scaltre.to>
2026-03-29 08:37:25 -0400

    fix(cmp): preserve lsp cue ordering

--- lua/downstage/init.lua
+++ lua/downstage/init.lua
@@ -2,6 +2,7 @@ local M = {}
 local state = {
   lsp = false,
   cmp = false,
+  cmp_autocmd = false,
 }
 
 local function setup_lsp()
@@ -18,12 +19,63 @@ local function setup_cmp()
   if not ok then
     return
   end
+  local compare = require("cmp.config.compare")
 
   cmp.setup.filetype("downstage", {
+    completion = {
+      autocomplete = {
+        cmp.TriggerEvent.InsertEnter,
+        cmp.TriggerEvent.TextChanged,
+      },
+      keyword_length = 0,
+    },
     sources = {
-      { name = "nvim_lsp" },
+      {
+        name = "nvim_lsp",
+        keyword_length = 0,
+      },
+    },
+    sorting = {
+      priority_weight = 1,
+      comparators = {
+        compare.sort_text,
+        compare.order,
+      },
     },
   })
+
+  if state.cmp_autocmd then
+    return
+  end
+
+  local group = vim.api.nvim_create_augroup("DownstageCmp", { clear = true })
+  vim.api.nvim_create_autocmd({ "InsertEnter", "TextChangedI" }, {
+    group = group,
+    callback = function(args)
+      if vim.bo[args.buf].filetype ~= "downstage" then
+        return
+      end
+
+      local line = vim.api.nvim_get_current_line()
+      if line ~= "" then
+        return
+      end
+
+      vim.schedule(function()
+        if vim.api.nvim_get_current_buf() ~= args.buf then
+          return
+        end
+        if vim.bo[args.buf].filetype ~= "downstage" then
+          return
+        end
+        if vim.api.nvim_get_current_line() ~= "" then
+          return
+        end
+        cmp.complete()
+      end)
+    end,
+  })
+  state.cmp_autocmd = true
 end
 
 function M.setup(opts)
